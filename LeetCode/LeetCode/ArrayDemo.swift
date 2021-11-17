d2a4aa528776c6778a7de92561ffd4519f8188c6
liliguang <445948630@qq.com>
2021-11-17 23:17:22 +0800

    存在重复元素

--- LeetCode/LeetCode/ArrayDemo.swift
+++ LeetCode/LeetCode/ArrayDemo.swift
@@ -77,4 +77,22 @@ class ArrayDemo {
         print(nums);
     }
     
+    
+    // 存在重复元素
+    static func containsDuplicate(_ nums: [Int]) -> Bool {
+        // 创建一个哈希表
+        var hashMap : Dictionary<String, Int> = [:];
+        var isContainsDuplicate = false;
+        var index = 0;
+        while ( index < nums.count ) {
+            if ( hashMap[String(nums[index])] != nil ) {
+                isContainsDuplicate = true;
+                break;
+            }
+            hashMap.updateValue(index, forKey: String(nums[index]))
+            index += 1;
+        }
+        return isContainsDuplicate;
+
+    }
 }
--- LeetCode/LeetCode/main.swift
+++ LeetCode/LeetCode/main.swift
@@ -8,11 +8,19 @@
 import Foundation
 
 
-print("\n===============")
-var nums = [1,2,3,4,5,6,7,8,9,10,11,12,13];
 
-nums = [1,2]
-print("ArrayDemo.maxProfit(nums) \(ArrayDemo.rotate(&nums, 5))")
+print("\n===============")
+var nums = [1,2,3,1];
+print("ArrayDemo.maxProfit(nums) \(ArrayDemo.containsDuplicate(nums))")
+
+
+
+//
+//print("\n===============")
+//var nums = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+//
+//nums = [1,2]
+//print("ArrayDemo.maxProfit(nums) \(ArrayDemo.rotate(&nums, 5))")
 
 
 //print("\n===============")
