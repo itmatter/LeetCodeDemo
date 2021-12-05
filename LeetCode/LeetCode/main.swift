5ef4f03f685fafcdf945740f299299fd8dc866e6
liliguang <445948630@qq.com>
2021-12-05 16:19:22 +0800

    剑指 Offer 32 - III. 从上到下打印二叉树 III

--- LeetCode/LeetCode/main.swift
+++ LeetCode/LeetCode/main.swift
@@ -8,13 +8,20 @@
 import Foundation
 
 
+let treeNode7   = TreeNode(7, nil, nil)
+let treeNode6   = TreeNode(6, nil, nil)
+
 let treeNode5   = TreeNode(5, nil, nil)
 let treeNode4   = TreeNode(4, nil, nil)
 
-let treeNode3   = TreeNode(3, treeNode4, treeNode5)
-let treeNode2   = TreeNode(2, nil, nil)
+let treeNode3   = TreeNode(3, treeNode6, treeNode7)
+let treeNode2   = TreeNode(2, treeNode4, treeNode5)
 
 let treeNode1   = TreeNode(1, treeNode2, treeNode3)
 
-print(Offer32.levelOrder(treeNode1))
-print(Offer32.levelOrder2(treeNode1))
+
+
+//print(Offer32.levelOrder(treeNode1))
+//print(Offer32.levelOrder2(treeNode1))
+print(Offer32.levelOrder3(treeNode1))
+
--- LeetCode/LeetCode/剑指 Offer/Day05_Offer32.swift
+++ LeetCode/LeetCode/剑指 Offer/Day05_Offer32.swift
@@ -80,4 +80,63 @@ class Offer32 {
         }
         return result
     }
+    
+    
+    // 从上到下打印二叉树 III
+    // 请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺序打印，
+    // 第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，其他行以此类推。
+    /**
+         1
+        /    \
+       2       3
+      /  \       /  \
+     4   5      6   7
+     */
+    // [[1] , [3,2], [4,5,6,7]]
+    static func levelOrder3(_ root: TreeNode?) -> [[Int]] {
+        if (root == nil ) {
+            return []
+        }
+        var queue = [TreeNode]()
+        queue.append(root!)
+        var result = [[Int]]()
+        var leftToRight = false
+        
+        while (!queue.isEmpty) {
+            var size = queue.count
+            var vals = [Int]()
+            leftToRight = !leftToRight
+            while (size > 0) {
+                if ( leftToRight ) {
+                    let node = queue.popLast()!
+                    size -= 1
+                    vals.append(node.val)
+                    
+                    if (node.left != nil) {
+                        queue.insert(contentsOf: [node.left!], at: 0)
+                    }
+                    
+                    if (node.right != nil) {
+                        queue.insert(contentsOf: [node.right!], at: 0)
+                    }
+                   
+                } else {
+           
+                    let node = queue[0]
+                    queue.remove(at: 0)
+                    size -= 1
+                    vals.append(node.val)
+                    
+                    if (node.right != nil) {
+                        queue.append(node.right!)
+                    }
+                    if (node.left != nil) {
+                        queue.append(node.left!)
+                    }
+                }
+            }
+            result.append(vals)
+        }
+        return result
+    }
 }
